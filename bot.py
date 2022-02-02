ce439160a53d1960715f0581d0efa074435f1c08
Chris Milios <cmilios@hotmail.com>
2022-02-03 01:47:18 +0200

    changes

--- bot.py
+++ bot.py
@@ -2,33 +2,17 @@ from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import requests
 import re
 import os
+
 PORT = int(os.environ.get('PORT', 5000))
 TOKEN = os.environ["TOKEN"]
-API_ENDPOINT = 'https://dog.ceo/api/breeds/image/random'
-
-
-def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler('bop', bop))
-
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://your-app-name.herokuapp.com/' + TOKEN)
-    updater.idle()
-
-def bop(update, context):
-  url = get_image_url()
-  chat_id = update.message.chat.id
-  context.bot.send_photo(chat_id=chat_id, photo=url)
 
 
 def get_url():
-    contents = requests.get(API_ENDPOINT).json()
+    contents = requests.get('https://dog.ceo/api/breeds/image/random').json()
     url = contents['message']
     return url
+
+
 def get_image_url():
     allowed_extension = ['jpg', 'jpeg', 'png']
     file_extension = ''
@@ -36,6 +20,25 @@ def get_image_url():
         url = get_url()
         file_extension = re.search("([^.]*)$", url).group(1).lower()
     return url
+
+
+def bop(update, context):
+    url = get_image_url()
+    chat_id = update.message.chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=url)
+
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop', bop))
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://boogram.herokuapp.com/' + TOKEN)
+
+    updater.idle()
 
 
 if __name__ == '__main__':
